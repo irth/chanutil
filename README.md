ad543c44aa4b6747312d145e186b781c6250446c
Wojciech Kwolek <wojciech@kwolek.io>
2023-02-18 19:45:05 +0100

    add docs to readme, document returns from get

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 Some utilities for interacting with channels that I use in my code.
 
+- `Get` - receive from a channel in a blocking manner, but return if a context expires.
+- `Put` - the same, for sending to a channel.
+
 Currently, it's so small, that it probably makes no sense to use it. Feel free
 to just steal the code you want, no need to introduce unnecessary dependency
 on this lib. It's MIT.
--- chanutil.go
+++ chanutil.go
@@ -17,8 +17,11 @@ func Put[T any](ctx context.Context, ch chan<- T, v T) error {
 }
 
 // Get tries to read a message from a channel, blocking until it succeeds or the
-// context gets cancelled or expires. Returns ctx.Err() if the message was not
-// received successfuly.
+// context gets cancelled or expires.
+//
+// Returns a pointer to the message if it was received successfuly (nil
+// otherwise), a bool equal to true if the channel isn't closed, and ctx.Err()
+// if the context expired/got cancelled.
 func Get[T any](ctx context.Context, ch <-chan T) (*T, bool, error) {
 	select {
 	case <-ctx.Done():
