c2154c7f82bd5dd5ff2d79e9a8bb80665fd3e91b
Wojciech Kwolek <wojciech@kwolek.io>
2023-02-18 19:36:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# chanutil
+
+Some utilities for interacting with channels that I use in my code.
+
+Currently, it's so small, that it probably makes no sense to use it. Feel free
+to just steal the code you want, no need to introduce unnecessary dependency
+on this lib. It's MIT.
---
+++ chanutil.go
@@ -0,0 +1,29 @@
+package chanutil
+
+import (
+	"context"
+)
+
+// Put tries to put a message in a channel, blocking until it succeeds or the
+// context gets cancelled or expires. Returns ctx.Err() if the message was not
+// sent successfuly.
+func Put[T any](ctx context.Context, ch chan<- T, v T) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case ch <- v:
+		return nil
+	}
+}
+
+// Get tries to read a message from a channel, blocking until it succeeds or the
+// context gets cancelled or expires. Returns ctx.Err() if the message was not
+// received successfuly.
+func Get[T any](ctx context.Context, ch <-chan T) (*T, bool, error) {
+	select {
+	case <-ctx.Done():
+		return nil, false, ctx.Err()
+	case v, more := <-ch:
+		return &v, more, nil
+	}
+}
